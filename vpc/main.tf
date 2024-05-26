017120b39cec81fa6f44046dda842d879418bb2a
jsmarzaro <61018222+jsmarzaro@users.noreply.github.com>
2024-05-25 23:45:52 -0300

    ajuste variaveis-vpc

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,23 +2,24 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.8.1"
 
-  name = "lab-vpc"
-  cidr = "10.0.0.0/16"
+  name = var.vpc_name
+  cidr = var.vpc_cidr
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = var.availability_zones
+  private_subnets = var.priv_subnets
+  public_subnets  = var.pub_subnets
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
 
-  tags = {
-    Terraform   = "true"
-    Environment = "hml"
-    Project     = "lab-eks"
-  }
+  tags = var.tags
 
   public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
+
+  private_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"           = "1"
   }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -13,7 +13,46 @@ variable "profile" {
   default = "default"
 }
 
+variable "vpc_name" {
+  type    = string
+  default = "lab-vpc"
+}
+
 variable "cluster_name" {
   type    = string
   default = "eks-lab-smarzaro"
 }
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "priv_subnets" {
+  description = "List of private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "pub_subnets" {
+  description = "List of public subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+variable "tags" {
+  description = "Map of tags to apply to resources"
+  type        = map(string)
+  default = {
+    Terraform   = "true"
+    Environment = "hml"
+    Project     = "lab-eks"
+  }
+}
+
